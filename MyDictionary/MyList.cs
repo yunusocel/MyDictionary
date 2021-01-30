d9c6d795f6528c5006c6902653907c354b4fed47
yunus öcel <yunusocel.45@gmail.com>
2021-01-30 18:25:08 +0300

    Add project files.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<T>
+    {
+        T[] _array;
+        T[] tempArray;
+        public MyList()
+        {
+            _array = new T[0]; 
+        }
+
+        public void Add(T item)
+        {
+            tempArray = _array;
+            _array = new T[_array.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _array[i] = tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+
+        public int Count
+        {
+            get { return _array.Length; }
+           
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Manisa");
+            myList.Add("Ankara");
+            myList.Add("İstanbul");
+            Console.WriteLine(myList.Count);
+            
+        }
+    }
+}
